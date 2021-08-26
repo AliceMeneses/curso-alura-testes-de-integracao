2b054285f75e1493037a028c3756fd094d5d0518
AliceMeneses <alicesilvademeneses@gmail.com>
2021-08-26 18:58:56 -0300

    Refatorar e adicionar teste naoDeveriaEncontrarUsuarioNaoCadastrado()

--- src/test/java/br/com/alura/leilao/dao/UsuarioDaoTest.java
+++ src/test/java/br/com/alura/leilao/dao/UsuarioDaoTest.java
@@ -3,7 +3,10 @@ package br.com.alura.leilao.dao;
 import static org.junit.Assert.*;
 
 import javax.persistence.EntityManager;
+import javax.persistence.NoResultException;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import br.com.alura.leilao.model.Usuario;
@@ -11,20 +14,48 @@ import br.com.alura.leilao.util.JPAUtil;
 
 class UsuarioDaoTest {
 
-	@Test
-	void deveriaBuscarUsuarioPorUsername() {
-		EntityManager entityManager = JPAUtil.getEntityManager();
-		UsuarioDao usuarioDao = new UsuarioDao(entityManager);
-		
-		
-		Usuario usuario = new Usuario("alice", "alice@gmail.com", "123456");
-		
-		entityManager.getTransaction().begin();
-		entityManager.persist(usuario);
-		entityManager.getTransaction().commit();
+	private EntityManager entityManager;
+	private UsuarioDao usuarioDao;
 
+	@BeforeEach
+	public void beforeEach() {
+		
+		entityManager = JPAUtil.getEntityManager();
+		usuarioDao = new UsuarioDao(entityManager);
+		entityManager.getTransaction().begin();
+		
+	}
+
+	@AfterEach
+	public void afterEach() {
+		
+		entityManager.getTransaction().rollback();
+		
+	}
+
+	@Test
+	void deveriaEncontrarUsuarioCadastrado() {
+
+		Usuario usuario = criarUsuario();
 		usuario = usuarioDao.buscarPorUsername(usuario.getNome());
 		assertNotNull(usuario);
+		
+	}
+
+	@Test
+	public void naoDeveriaEncontrarUsuarioNaoCadastrado() {
+		
+		criarUsuario();
+		assertThrows(NoResultException.class,() -> usuarioDao.buscarPorUsername("fulano"));
+		
+	}
+
+	public Usuario criarUsuario() {
+		
+		Usuario usuario = new Usuario("alice", "alice@gmail.com", "123456");
+		entityManager.persist(usuario);
+		
+		return usuario;
 	}
 
 }
